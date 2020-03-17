721b70da57474c1c062927d9207c1d456959d14a
Shilton <shilton.sistemas@gmail.com>
2020-03-17 13:45:25 -0300

    Creating SignUp Page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
+import logo from '~/assets/logo.png';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+import Background from '~/components/Background';
 
-// import { Container } from './styles';
+export default function SignUp({navigation}) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
 
-export default function SignUp() {
-  return <Text>SignUp</Text>;
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu nome"
+          />
+          <FormInput
+            icon="mail-outline" //email
+            keyboardType="email-address" //@
+            autoCorrect={false} //Não corrigir auto
+            autoCapitalize="none" //minusculo
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry //****
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Login</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 }
