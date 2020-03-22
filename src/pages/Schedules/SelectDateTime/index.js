9b30de81d5f865c172bc29aa1c5dcdf481f533a3
Shilton <shilton.sistemas@gmail.com>
2020-03-22 15:48:34 -0300

    Provider selection listing

--- src/pages/Schedules/SelectDateTime/index.js
+++ src/pages/Schedules/SelectDateTime/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import Background from '~/components/Background';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {TouchableOpacity} from 'react-native';
 
 // import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <View />;
+  return <Background />;
 }
+
+SelectDateTime.navigationOptions = ({navigation}) => ({
+  title: 'Selecione a data e horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('SelectProvider');
+      }}>
+      <Icon name="chevron-left" size={25} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
--- src/pages/Schedules/SelectProvider/index.js
+++ src/pages/Schedules/SelectProvider/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Background from '~/components/Background';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {TouchableOpacity} from 'react-native';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import {Container, ProvidersList, Provider, Avatar, Name} from './styles';
 
-export default function SelectProvider() {
-  return <Background></Background>;
+export default function SelectProvider({navigation}) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+      setProviders(response.data);
+    }
+    loadProviders();
+  }, [providers]);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({item: provider}) => (
+            <Provider
+              onPress={() => navigation.navigate('SelectDateTime', {provider})}>
+              <Avatar
+                source={{
+                  uri: `https://api.adorable.io/avatars/40/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({navigation}) => ({
